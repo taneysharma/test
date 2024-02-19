bc88da10e47e553da6346d5334f418c151393968
taneysharma <taney219621@gmail.com>
2024-02-19 11:36:42 +0530

    Index file created

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Hello World!!");
+
+// let favouriteNumber = 3;
+// console.log(favouriteNumber) ;
+// favouriteNumber = 4 ;
+// console.log(favouriteNumber) ;
+
+// let a = 1 ;
+// let b = 2 ;
+// let c = 3 ;
+// console.log(a,b,c) ;
+
+// a= 3 ;
+// let d = a ;
+// console.log(a);
+// console.log(d);
+
+// let int = 2;
+// let float = 2.4;
+// console.log(typeof int);
+// console.log(typeof float);
+// console.log(float + int);
+// console.log(1 + int);
+// console.log(1 / int);
+// console.log((1 + 2) * 3);
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// console.log((a + b) / c);  
+
+// let a = 1.2 ;
+// let b = 2.2 ;
+// console.log(a+b) ; // result is not precise due to the floating point numbers are represented in binary and sometimes they have Rounding errors
+// console.log(a*b) ; // no rounding errors
+
+
+// String Type (can be inside single and double quotes)
+
+let a = 'John says "Hi" ' ;
+console.log(a) ; 
+console.log(typeof a) ; 
+
